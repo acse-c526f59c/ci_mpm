332bf73ce9e6c20eaafd379cee9666bab5e53747
acse-c526f59c <ch322@ic.ac.uk>
2022-10-18 21:22:10 +0100

    modified

--- simple_functions/constants.py
+++ simple_functions/constants.py
@@ -1,12 +1,15 @@
-import numpy as np
+from numpy import sqrt
+from simple_functions.functions1 import factorial
+from functools import lru_cache
 
-from simple_functions import pi
+__all__ = ['pi']
 
 
-class TestPi(object):
-    '''Class to test our constants are computed correctly'''
+def pi(terms=1):
+    return 1./(2.*sqrt(2.)/9801.*rsum(terms))
 
-    def test_pi(self):
-        '''Test computation of pi'''
-        my_pi = pi(2)
-        assert np.isclose(my_pi, np.pi, atol=1e-12)
+
+@cache
+def rsum(n):
+    t = factorial(4*n)*(1103+26390*n)/(factorial(n)**4*396**(4*n))
+    return t + rsum(n-1) if n else t
